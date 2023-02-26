f789cfeb29d0d5a07d8fcbe884de9c45bf6deba9
ChisKien <chiskien03214@gmail.com>
2023-02-26 22:28:00 +0700

    SelectionSort.java

--- src/main/java/org/chiskien/Main.java
+++ src/main/java/org/chiskien/Main.java
@@ -6,33 +6,35 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    private static final String DATA = "src/main/java/org/chiskien/data.txt";
+    private static final String DATA = "src/main/resources/data.txt";
 
     public static void main(String[] args) throws IOException {
-        var arrayData = getDataFromFile(DATA);
-        System.out.println(Arrays.toString(arrayData));
+        var data = getDataFromFile(DATA);
+        System.out.println(data);
         SelectionSort selectionSort = SelectionSort.getInstance();
-        System.out.println(selectionSort.findSmallestItem(arrayData));
+        System.out.println("The index has the smallest value is:" + selectionSort.findSmallestItem(data));
+        System.out.println("Selection sort: " + selectionSort.selectionSort(data));
     }
 
-    private static int[] getDataFromFile(String uri) throws IOException {
+    private static List<Integer> getDataFromFile(String uri) throws IOException {
         Path path = Paths.get(uri);
         List<String> lines = Files.readAllLines(path);
         String[] items = new String[0];
         for (String line : lines) {
             items = line.split(",");
         }
-        int[] arrayInput = new int[items.length];
-        for (int i = 0; i < arrayInput.length; i++) {
-            arrayInput[i] = Integer.parseInt(items[i]);
+        List<Integer> listInteger = new ArrayList<>(items.length);
+        for (int i = 0; i < items.length; i++) {
+            listInteger.add(Integer.parseInt(items[i]));
         }
-        return arrayInput;
+        return listInteger;
     }
-
+    private static void writeToFile(String uri, List<Integer> data)
     private static void generateRandomArray(int arraySize, int min, int max, String regex) {
 
     }
--- src/main/java/org/chiskien/sorting/SelectionSort.java
+++ src/main/java/org/chiskien/sorting/SelectionSort.java
@@ -1,5 +1,8 @@
 package org.chiskien.sorting;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SelectionSort {
 
     private SelectionSort() {
@@ -14,18 +17,27 @@ public class SelectionSort {
         return instance;
     }
 
-    public int[] selectionSort(int[] arrayInput) {
-        return arrayInput;
+    public List<Integer> selectionSort(List<Integer> inputList) {
+        int n = inputList.size();
+        List<Integer> result = new ArrayList<>(n);
+        int smallestOfArrayInput;
+        for (int i = 0; i < n; i++) {
+            smallestOfArrayInput = findSmallestItem(inputList);
+            result.add(inputList.get(smallestOfArrayInput));
+            inputList.remove(smallestOfArrayInput);
+        }
+        return result;
     }
 
-    public int findSmallestItem(int[] arrayInput) {
-        int smallest = arrayInput[0];
+    public int findSmallestItem(List<Integer> inputList) {
+        int smallest = inputList.get(0);
         int smallestIndex = 0;
-        for (int i = 0; i < arrayInput.length; i++) {
-            if (arrayInput[i] < smallest) {
+        for (int i = 0; i < inputList.size(); i++) {
+            if (inputList.get(i) < smallest) {
+                smallest = inputList.get(i);
                 smallestIndex = i;
             }
         }
-        return smallest;
+        return smallestIndex;
     }
 }
