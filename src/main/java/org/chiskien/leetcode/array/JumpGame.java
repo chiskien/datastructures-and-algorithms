e9452a5f72063309401bd864be86da7e6c3429e8
ChisKien <chiskien03214@gmail.com>
2023-06-20 14:06:43 +0700

    JumpGame

--- src/main/java/org/chiskien/leetcode/array/JumpGame.java
+++ src/main/java/org/chiskien/leetcode/array/JumpGame.java
@@ -3,16 +3,16 @@ package org.chiskien.leetcode.array;
 public class JumpGame {
     public static void main(String[] args) {
         int[] a = new int[]{2, 3, 1, 1, 4};
-        int[] b = new int[]{3, 2, 1, 0, 4};
         System.out.println(canJump(a));
     }
 
     public static boolean canJump(int[] nums) {
-        int step = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (step < i) return false;
-            step = Math.max(step, i + nums[i]);
+        int furthestReachSoFar = 0;
+        int lastIndex = nums.length - 1;
+        for (int i = 0; i <= furthestReachSoFar && furthestReachSoFar < lastIndex; i++) {
+            //the furthest index can jump is i + nums[i]
+            furthestReachSoFar = Math.max(furthestReachSoFar, i + nums[i]);
         }
-        return true;
+        return furthestReachSoFar >= lastIndex;
     }
 }
