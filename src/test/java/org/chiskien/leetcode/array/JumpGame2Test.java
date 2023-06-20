44073886daecc87c0cefbbe30fa15734b62a64c9
ChisKien <chiskien03214@gmail.com>
2023-06-20 16:59:27 +0700

    JumpGame 2

---
+++ src/test/java/org/chiskien/leetcode/array/JumpGame2Test.java
@@ -0,0 +1,41 @@
+package org.chiskien.leetcode.array;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JumpGame2Test {
+    JumpGame2 jumpGame2;
+
+    @BeforeEach
+    void setUp() {
+        jumpGame2 = new JumpGame2();
+    }
+
+    @Test
+    void minimumJumpToLast() {
+        int[] nums = new int[]{2, 3, 1, 1, 4};
+        int minimumNumber = jumpGame2.minimumJumpToLast(nums);
+        assertEquals(2, minimumNumber);
+    }
+
+    @Test
+    void minimumJumpToLast2() {
+        int[] nums = new int[]{ 1, 3, 1, 1, 2, 3, 5, 7, 8};
+        int minimumNumber = jumpGame2.minimumJumpToLast(nums);
+        assertEquals(4, minimumNumber);
+    }
+    @Test
+    void minimumJumpToLast3() {
+        int[] nums = new int[]{1,2,1,1,1};
+        int minimumNumber = jumpGame2.minimumJumpToLast(nums);
+        assertEquals(3, minimumNumber);
+    }
+    @Test
+    void minimumJumpToLast4() {
+        int[] nums = new int[]{7,0,9,6,9,6,1,7,9,0,1,2,9,0,3};
+        int minimumNumber = jumpGame2.minimumJumpToLast(nums);
+        assertEquals(2, minimumNumber);
+    }
+}
