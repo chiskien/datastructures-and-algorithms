6b31e37ff701dd0b47078a261f6ef117a8c07e67
ChisKien <chiskien03214@gmail.com>
2023-07-16 08:56:50 +0700

    Path Sum Binary Tree

---
+++ src/main/java/org/chiskien/leetcode/binarytree/TreeNodeHelper.java
@@ -0,0 +1,20 @@
+package org.chiskien.leetcode.binarytree;
+
+public class TreeNodeHelper {
+    public TreeNode root;
+
+    public TreeNodeHelper(TreeNode root) {
+        this.root = root;
+    }
+
+    public TreeNodeHelper() {
+    }
+
+    public TreeNode build(Iterable<TreeNode> collections) {
+        return null;
+    }
+
+    public TreeNode build(int[] nodes) {
+        return null;
+    }
+}
---
+++ src/test/java/org/chiskien/leetcode/binarytree/PathSumTest.java
@@ -0,0 +1,19 @@
+package org.chiskien.leetcode.binarytree;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PathSumTest {
+    PathSum pathSum;
+
+    @BeforeEach
+    void setUp() {
+        pathSum = new PathSum();
+    }
+
+    @Test
+    void pathSum1() {
+
+    }
+
+}
