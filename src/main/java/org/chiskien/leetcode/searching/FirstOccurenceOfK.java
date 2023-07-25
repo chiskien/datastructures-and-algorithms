2876b0a9a6d4c416a8717f30dafd39be8b9dda55
ChisKien <chiskien03214@gmail.com>
2023-07-25 16:25:43 +0700

    FirstOccurrenceOfK

---
+++ src/main/java/org/chiskien/leetcode/searching/FirstOccurenceOfK.java
@@ -0,0 +1,23 @@
+package org.chiskien.leetcode.searching;
+
+public class FirstOccurenceOfK {
+
+
+    public int searchFirstOccurenceOfK(int[] nums, int k) {
+        int left = 0;
+        int right = nums.length - 1;
+        int result = -1;
+        while (left <= right) {
+            int mid = left + ((right - left) / 2);
+            if (nums[mid] > k) {
+                right = mid - 1;
+            } else if (nums[mid] < k) {
+                left = mid + 1;
+            } else {
+                result = mid;
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+}
