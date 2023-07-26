7bb144452ea00822dabb1f5de6f51c746aadc0b0
ChisKien <chiskien03214@gmail.com>
2023-07-26 12:43:03 +0700

    RansomNote

--- src/main/java/org/chiskien/leetcode/hashmap/RansomNote.java
+++ src/main/java/org/chiskien/leetcode/hashmap/RansomNote.java
@@ -10,17 +10,32 @@ public class RansomNote {
     }
 
     public static boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character, Integer> hashMap = new HashMap<>();
-        for (char c : magazine.toCharArray()) {
-            hashMap.put(c, hashMap.getOrDefault(c, 0) + 1);
-        }
-        for (char c : ransomNote.toCharArray()) {
-            if (hashMap.containsKey(c) || hashMap.get(c) < 1) {
-                return false;
+        //compute the frequency for all characters in ransomNote
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char c = ransomNote.charAt(i);
+            if (frequencyMap.containsKey(c)) {
+                frequencyMap.put(c, frequencyMap.get(c) + 1);
             } else {
-                hashMap.put(c, hashMap.getOrDefault(c, 0) - 1);
+                frequencyMap.put(c, 1);
             }
         }
-        return true;
+        //Check if the characters in magazine can cover characters in ransomNote
+        for (Character c : magazine.toCharArray()) {
+            if (frequencyMap.containsKey(c)) {
+                frequencyMap.put(c, frequencyMap.get(c) - 1);
+                if (frequencyMap.get(c) == 0) {
+                    //reduce frequency of character in hashmap
+                    frequencyMap.remove(c);
+
+                    //if all characters are matched
+                    if (frequencyMap.isEmpty()) {
+                        break;
+                    }
+                }
+            }
+        }
+        //If map is empty so that the magazine covers all characters in ransomNote
+        return frequencyMap.isEmpty();
     }
 }
