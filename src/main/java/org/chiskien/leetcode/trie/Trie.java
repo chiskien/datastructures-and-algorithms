ea130565aef90b02dedf3c2cfb2c9e965a80819e
ChisKien <chiskien03214@gmail.com>
2023-09-07 16:03:00 +0700

    Implementing Trie (Prefix Tree) come with Unit Tests.

---
+++ src/main/java/org/chiskien/leetcode/trie/Trie.java
@@ -0,0 +1,43 @@
+package org.chiskien.leetcode.trie;
+
+public class Trie {
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode current = root;
+        for (Character c : word.toCharArray()) {
+            if (!current.childrens.containsKey(c)) {
+                current.childrens.put(c, new TrieNode());
+            }
+            current = current.childrens.get(c);
+        }
+        current.isWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode current = root;
+        for (Character c : word.toCharArray()) {
+            if (!current.childrens.containsKey(c)) {
+                return false;
+            }
+            current = current.childrens.get(c);
+        }
+        return current.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode current = root;
+        for (Character c : prefix.toCharArray()) {
+            if (!current.childrens.containsKey(c)) {
+                return false;
+            }
+            current = current.childrens.get(c);
+        }
+        return true;
+    }
+
+}
--- src/main/java/org/chiskien/practice/Top150.java
+++
@@ -1,7 +0,0 @@
-package org.chiskien.practice;
-
-public class Top150 {
-
-    //88. Merge Sorted Array
-
-}
