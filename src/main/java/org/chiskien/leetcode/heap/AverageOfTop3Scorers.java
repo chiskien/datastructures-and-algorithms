e090c81a61e56b11c61349d869968e0997333686
ChisKien <chiskien03214@gmail.com>
2023-09-09 16:44:17 +0700

    AverageOfTop3Scorers.java

--- src/main/java/org/chiskien/leetcode/heap/AverageOfTop3Scorers.java
+++ src/main/java/org/chiskien/leetcode/heap/AverageOfTop3Scorers.java
@@ -1,4 +1,15 @@
 package org.chiskien.leetcode.heap;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class AverageOfTop3Scorers {
+
+
+    public static String findStudentsWithHighestBestofThreeScores(
+            Iterator<Object> nameScoreData) {
+        return "";
+    }
 }
