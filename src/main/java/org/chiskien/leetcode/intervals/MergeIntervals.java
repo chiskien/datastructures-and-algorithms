37ab9079f8b654ebc1fdd7311138706995296a3c
ChisKien <chiskien03214@gmail.com>
2023-09-10 09:20:49 +0700

    Merging Intervals

--- src/main/java/org/chiskien/leetcode/intervals/MergeIntervals.java
+++ src/main/java/org/chiskien/leetcode/intervals/MergeIntervals.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 public class MergeIntervals {
 
     public void merge(int[][] intervals) {
-
+        int i = 0;
+        while (i < intervals.length - 1 && intervals[i + 1][0] < intervals[i][1]) {
+            intervals[i][1] = intervals[i + 1][1];
+            i++;
+        }
     }
 }
