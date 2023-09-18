407c481c6c45491d17be8091fccf9a7b42fd7cf8
ChisKien <chiskien03214@gmail.com>
2023-09-18 18:12:44 +0700

    K weakest row using Priority Queue (Heap)

--- src/main/java/org/chiskien/datastructures_algorithms/leetcode/array/ValidSudoku.java
+++ src/main/java/org/chiskien/datastructures_algorithms/leetcode/array/ValidSudoku.java
@@ -1,8 +1,8 @@
 package org.chiskien.datastructures_algorithms.leetcode.array;
 
+import java.util.Collections;
+
 public class ValidSudoku {
-
-
     public boolean isValidSudoku(char[][] board) {
         return false;
     }
