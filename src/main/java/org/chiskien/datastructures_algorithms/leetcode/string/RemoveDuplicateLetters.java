4387549a42d1779897353d849d09ca24195d93ca
ChisKien <chiskien03214@gmail.com>
2023-09-26 09:49:50 +0700

    [LeetCode] 26/09: Remove Duplicate Letters

---
+++ src/main/java/org/chiskien/datastructures_algorithms/leetcode/string/RemoveDuplicateLetters.java
@@ -0,0 +1,25 @@
+package org.chiskien.datastructures_algorithms.leetcode.string;
+
+import java.util.*;
+
+public class RemoveDuplicateLetters {
+    public static void main(String[] args) {
+        removeDuplicateLetters("aabbccdd");
+    }
+
+    public static String removeDuplicateLetters(String s) {
+        StringBuilder builder = new StringBuilder();
+        boolean[] visited = new boolean[26];
+        int[] lastIndex = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            lastIndex[s.charAt(i) - 'a'] = i;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (!visited[s.charAt(i) - 'a']) {
+                int lastCharacterIndex = builder.length() - 1;
+
+            }
+        }
+        return "";
+    }
+}
