ff0f0238d73622388a34340b878eb4b6616c102a
ChisKien <chiskien03214@gmail.com>
2023-10-05 17:45:27 +0700

    Test if linked list is palindrome

--- src/main/java/org/chiskien/datastructures_algorithms/leetcode/linked_list/PalindromeLinkedList.java
+++ src/main/java/org/chiskien/datastructures_algorithms/leetcode/linked_list/PalindromeLinkedList.java
@@ -1,4 +1,16 @@
 package org.chiskien.datastructures_algorithms.leetcode.linked_list;
 
 public class PalindromeLinkedList {
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) return true;
+
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        //Compare first half and the reversed second half lists
+        return false;
+    }
 }
