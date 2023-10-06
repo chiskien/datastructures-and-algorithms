d8063e4641e64214017b6050642a9f5527fd897f
ChisKien <chiskien03214@gmail.com>
2023-10-06 14:16:24 +0700

    Rename

---
+++ src/main/java/org/chiskien/datastructures_algorithms/leetcode/linked_list/implementations/DoublyNode.java
@@ -0,0 +1,18 @@
+package org.chiskien.datastructures_algorithms.leetcode.linked_list.implementations;
+
+class DoublyNode<T> {
+    public DoublyNode<T> next;
+    public DoublyNode<T> prev;
+    public T data;
+
+    public DoublyNode(DoublyNode<T> next, DoublyNode<T> prev, T data) {
+        this.next = next;
+        this.prev = prev;
+        this.data = data;
+    }
+
+    public DoublyNode(T data) {
+        this.data = data;
+        next = prev = null;
+    }
+}
--- src/main/java/org/chiskien/datastructures_algorithms/leetcode/linked_list/implementations/SinglyLinkedList.java
+++ src/main/java/org/chiskien/datastructures_algorithms/leetcode/linked_list/implementations/SinglyLinkedList.java
@@ -5,8 +5,8 @@ import java.util.Iterator;
 public class SinglyLinkedList<T> implements LinkedList<T> {
 
     public int size;
-    public Node<T> head;
-    public Node<T> tail;
+    public SingleNode<T> head;
+    public SingleNode<T> tail;
 
     public SinglyLinkedList() {
         this.head = this.tail = null;
@@ -15,35 +15,35 @@ public class SinglyLinkedList<T> implements LinkedList<T> {
 
 
     public void addLast(T data) {
-        Node<T> node = new Node<>(data);
-        addLast(node);
+        SingleNode<T> singleNode = new SingleNode<>(data);
+        addLast(singleNode);
     }
 
 
 
 
     public void addFirst(T data) {
-        Node<T> node = new Node<>(data);
-        addFirst(node);
+        SingleNode<T> singleNode = new SingleNode<>(data);
+        addFirst(singleNode);
     }
 
-    public void addLast(Node<T> newNode) {
+    public void addLast(SingleNode<T> newSingleNode) {
         if (isEmpty()) {
-            head = tail = newNode;
+            head = tail = newSingleNode;
         } else if (head.getNext() == null) {
-            tail = newNode;
+            tail = newSingleNode;
             head.setNext(tail);
         } else {
-            tail.setNext(newNode);
-            tail = newNode;
+            tail.setNext(newSingleNode);
+            tail = newSingleNode;
         }
     }
-    public void addFirst(Node<T> newNode) {
+    public void addFirst(SingleNode<T> newSingleNode) {
         if (isEmpty()) {
-            head = tail = newNode;
+            head = tail = newSingleNode;
         } else {
-            newNode.setNext(head);
-            head = newNode;
+            newSingleNode.setNext(head);
+            head = newSingleNode;
         }
     }
 
@@ -59,7 +59,7 @@ public class SinglyLinkedList<T> implements LinkedList<T> {
 
     public T removeFirst() {
         if (isEmpty()) return null;
-        Node<T> n = head;
+        SingleNode<T> n = head;
         head = n.getNext();
         return n.getData();
     }
@@ -67,14 +67,14 @@ public class SinglyLinkedList<T> implements LinkedList<T> {
 
     public T removeLast() {
         if (isEmpty()) return null;
-        Node<T> n = head;
+        SingleNode<T> n = head;
         while (n.getNext() != tail) {
             n = n.getNext();
         }
-        Node<T> nodeTail = n.getNext();
+        SingleNode<T> singleNodeTail = n.getNext();
         n.setNext(null);
         tail = n;
-        return nodeTail.getData();
+        return singleNodeTail.getData();
     }
 
     public String traverse() {
@@ -82,7 +82,7 @@ public class SinglyLinkedList<T> implements LinkedList<T> {
         if (isEmpty()) {
             output.append("LinkedList is empty");
         } else {
-            Node<T> n = head;
+            SingleNode<T> n = head;
             while (n != null) {
                 output.append(n.getData()).append("-->");
                 n = n.getNext();
@@ -107,27 +107,27 @@ public class SinglyLinkedList<T> implements LinkedList<T> {
     }
 
     public void insertNodeAtIndex(T data, int k) {
-        Node<T> newNode = new Node<>(data);
+        SingleNode<T> newSingleNode = new SingleNode<>(data);
         int i = 1;
-        Node<T> n = head;
+        SingleNode<T> n = head;
         while (i != k) {
             n = n.getNext();
             i++;
         }
     }
 
-    public Node<T> searchNode(T key) {
+    public SingleNode<T> searchNode(T key) {
         if (isEmpty()) return null;
-        Node<T> n = head;
+        SingleNode<T> n = head;
         while (n != null && n.getData() != key) {
             n = n.getNext();
         }
         return n;
     }
 
-    public void removeDuplicateNode(Node<T> n) {
+    public void removeDuplicateNode(SingleNode<T> n) {
         while (n != null) {
-            Node<T> nextDistinct = n.getNext();
+            SingleNode<T> nextDistinct = n.getNext();
             while (nextDistinct != null && n.getData() != nextDistinct.getData()) {
                 nextDistinct = nextDistinct.getNext();
             }
@@ -142,33 +142,3 @@ public class SinglyLinkedList<T> implements LinkedList<T> {
     }
 }
 
-class Node<T> {
-    private Node<T> next;
-    private T data;
-
-    public Node(T data) {
-        this.next = null;
-        this.data = data;
-    }
-
-    public Node<T> getNext() {
-        return next;
-    }
-
-    public void setNext(Node<T> next) {
-        this.next = next;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
-
-    @Override
-    public String toString() {
-        return "Node with data: " + data;
-    }
-}
