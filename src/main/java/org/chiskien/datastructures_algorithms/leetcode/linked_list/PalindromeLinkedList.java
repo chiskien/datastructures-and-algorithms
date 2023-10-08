27265032b82285dc2b856f1630397fc3b7043db6
ChisKien <chiskien03214@gmail.com>
2023-10-08 10:02:15 +0700

    check if a linked-list is palindrome and unit tests

--- src/main/java/org/chiskien/datastructures_algorithms/leetcode/linked_list/PalindromeLinkedList.java
+++ src/main/java/org/chiskien/datastructures_algorithms/leetcode/linked_list/PalindromeLinkedList.java
@@ -3,14 +3,31 @@ package org.chiskien.datastructures_algorithms.leetcode.linked_list;
 public class PalindromeLinkedList {
 
     public boolean isPalindrome(ListNode head) {
-        if (head == null) return true;
+        //test the reverse of second half to first half
+        // if equal then is palindrome
 
+        //Reuse the reverse linked-list class
+        ReverseLinkedList reverseLinkedList = new ReverseLinkedList();
+
+        if (head == null) return true;
+        //finding the head of second half = slow
         ListNode slow = head, fast = head;
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
         }
         //Compare first half and the reversed second half lists
-        return false;
+        ListNode secondHalf = reverseLinkedList.reverseList(slow);
+        System.out.println(secondHalf);
+        ListNode firstHalf = head;
+        while (secondHalf != null && firstHalf != null) {
+            if (secondHalf.val != firstHalf.val) {
+                return false;
+            } else {
+                secondHalf = secondHalf.next;
+                firstHalf = firstHalf.next;
+            }
+        }
+        return true;
     }
 }
---
+++ src/test/java/org/chiskien/datastructures_algorithms/leetcode/linked_list/PalindromeLinkedListTest.java
@@ -0,0 +1,51 @@
+package org.chiskien.datastructures_algorithms.leetcode.linked_list;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PalindromeLinkedListTest {
+    PalindromeLinkedList palindromeLinkedList;
+
+    @BeforeEach
+    void setUp() {
+        palindromeLinkedList = new PalindromeLinkedList();
+    }
+
+    @Test
+    void isPalindrome() {
+        ListNode head = ListNode.build(new int[]{1, 2, 2, 1});
+        assertTrue(palindromeLinkedList.isPalindrome(head));
+    }
+
+    @Test
+    void emptyList() {
+        ListNode head = ListNode.build(new int[]{});
+        assertTrue(palindromeLinkedList.isPalindrome(head));
+    }
+
+    @Test
+    void oneNode() {
+        ListNode head = ListNode.build(new int[]{1});
+        assertTrue(palindromeLinkedList.isPalindrome(head));
+    }
+
+    @Test
+    void sameNode() {
+        ListNode head = ListNode.build(new int[]{1, 1, 1, 1, 1, 1, 1, 1});
+        assertTrue(palindromeLinkedList.isPalindrome(head));
+    }
+
+    @Test
+    void normalCase2() {
+        ListNode head = ListNode.build(new int[]{1, 2, 3, 4, 3, 2, 1});
+        assertTrue(palindromeLinkedList.isPalindrome(head));
+    }
+
+    @Test
+    void normalCase3() {
+        ListNode head = ListNode.build(new int[]{1, 2, 3, 4, 3, 2, 22});
+        assertFalse(palindromeLinkedList.isPalindrome(head));
+    }
+}
