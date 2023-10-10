4927bfec97a0eee4e502229b538cf70da4dd6a54
ChisKien <chiskien03214@gmail.com>
2023-10-10 09:26:41 +0700

    Add binary

--- src/main/java/org/chiskien/datastructures_algorithms/leetcode/bitmanipulation/AddBinary.java
+++ src/main/java/org/chiskien/datastructures_algorithms/leetcode/bitmanipulation/AddBinary.java
@@ -1,8 +1,14 @@
 package org.chiskien.datastructures_algorithms.leetcode.bitmanipulation;
 
 public class AddBinary {
+    public static void main(String[] args) {
+    }
 
-    public String addBinary(String a, String b) {
-        return "";
+    public static String addBinary(String a, String b) {
+        int a2 = Integer.parseInt(a, 2);
+        int b2 = Integer.parseInt(b, 2);
+        int sum = a2 + b2;
+
+        return Integer.toBinaryString(sum);
     }
 }
