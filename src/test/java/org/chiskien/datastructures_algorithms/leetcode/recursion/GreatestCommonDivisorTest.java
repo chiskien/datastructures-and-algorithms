bbbed2ef7549f0b0df661c72ad611827713b9383
ChisKien <chiskien03214@gmail.com>
2023-10-16 14:56:15 +0700

    GreatestCommonDivisor

---
+++ src/test/java/org/chiskien/datastructures_algorithms/leetcode/recursion/GreatestCommonDivisorTest.java
@@ -0,0 +1,14 @@
+package org.chiskien.datastructures_algorithms.leetcode.recursion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreatestCommonDivisorTest {
+    GreatestCommonDivisor gcd = new GreatestCommonDivisor();
+
+    @Test
+    void findGreatestCommonDivisor() {
+        assertEquals(12, gcd.findGreatestCommonDivisor(156, 36));
+    }
+}
