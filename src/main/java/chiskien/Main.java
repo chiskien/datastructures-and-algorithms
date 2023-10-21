cfc63cf81a39420e5ccf561d1b3f74e4f2f7e1b0
ChisKien <chiskien03214@gmail.com>
2023-10-21 16:23:19 +0700

    buildmatrix ultilities and ValidSudoku and unit tests

--- src/main/java/chiskien/Main.java
+++ src/main/java/chiskien/Main.java
@@ -7,28 +7,7 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Main {
-    private static final String DATA = "src/main/resources/data.txt";
-
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-
-    private static int[] getDataFromFile() throws IOException {
-        Path path = Paths.get(Main.DATA);
-        List<String> lines = Files.readAllLines(path);
-        String[] items = new String[0];
-        for (String line : lines) {
-            items = line.split(",");
-        }
-        int[] array = new int[items.length];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = Integer.parseInt(items[i]);
-        }
-        return array;
-    }
-
-
-    private static void generateRandomArray(int arraySize, int min, int max, String regex) {
-
+        System.out.println("Hello ChisKien");
     }
 }
