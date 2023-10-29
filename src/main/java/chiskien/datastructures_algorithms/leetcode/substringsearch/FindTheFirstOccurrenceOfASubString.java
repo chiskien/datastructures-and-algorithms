2a31ca3583d5dd7deab8e631ef835ab2865051d2
ChisKien <chiskien03214@gmail.com>
2023-10-29 10:56:36 +0700

    FindTheFirstOccurrenceOfASubString.java using Rabin-Karp Algorithm.

--- src/main/java/chiskien/datastructures_algorithms/leetcode/substringsearch/FindTheFirstOccurrenceOfASubString.java
+++ src/main/java/chiskien/datastructures_algorithms/leetcode/substringsearch/FindTheFirstOccurrenceOfASubString.java
@@ -1,12 +1,13 @@
 package chiskien.datastructures_algorithms.leetcode.substringsearch;
 
-public class FindTheFirstOccurenOfASubString {
+public class FindTheFirstOccurrenceOfASubString {
 
     //given two strings s and t, find the first occurrence of s in t
     //Rabin-Karp Algorithms
-
+    
     public static void main(String[] args) {
 
     }
 }
+
 
