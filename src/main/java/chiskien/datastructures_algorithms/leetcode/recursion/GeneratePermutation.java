be2dd5374ff61894fcec5fa5cb198de8bdd754de
ChisKien <chiskien03214@gmail.com>
2023-11-08 13:59:38 +0700

    Clean up code

--- src/main/java/chiskien/datastructures_algorithms/leetcode/recursion/GeneratePermutation.java
+++ src/main/java/chiskien/datastructures_algorithms/leetcode/recursion/GeneratePermutation.java
@@ -1,6 +1,12 @@
 package chiskien.datastructures_algorithms.leetcode.recursion;
 
+import java.util.List;
+
 public class GeneratePermutation {
 
+
+    public List<List<Integer>> generatePermutation(int[] nums) {
+        return null;
+    }
 
 }
