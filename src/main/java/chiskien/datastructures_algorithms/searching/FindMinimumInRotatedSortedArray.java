a3d7124f5748f2b7d670b657debdc92226ecd502
ChisKien <chiskien03214@gmail.com>
2023-11-26 21:50:52 +0700

    Clean up code

--- src/main/java/chiskien/datastructures_algorithms/searching/FindMinimumInRotatedSortedArray.java
+++ src/main/java/chiskien/datastructures_algorithms/searching/FindMinimumInRotatedSortedArray.java
@@ -8,7 +8,7 @@ public class FindMinimumInRotatedSortedArray {
         while (left < right) {
             int mid = ((right - left) / 2) + left;
             if (nums[mid] > nums[right]) {
-                //minimum must be in nums[mid + 1 : right + 1]
+                //the minimum must be in nums[mid + 1 : right + 1]
                 left = mid + 1;
             } else {  //nums[mid] < nums[right]
                 // minimum cannot be in nums[mid + 1 : right + 1]
