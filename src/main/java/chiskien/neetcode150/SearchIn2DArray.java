9737b8263e7e229d86e86af35e6599ef0f627bcb
ChisKien <chiskien03214@gmail.com>
2023-12-02 10:24:24 +0700

    SearchIn2DArray

---
+++ src/main/java/chiskien/neetcode150/SearchIn2DArray.java
@@ -0,0 +1,12 @@
+package chiskien.neetcode150;
+
+public class SearchIn2DArray {
+
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length;
+
+        return true;
+    }
+}
