add3df2a581b99c69d7e053c80a73fdfec4ff364
ChisKien <chiskien03214@gmail.com>
2023-12-08 09:17:26 +0700

    A cleaner version of ConstructStringFromBinaryTree.java

--- src/main/java/chiskien/datastructures_algorithms/binarytree/ConstructStringFromBinaryTree.java
+++ src/main/java/chiskien/datastructures_algorithms/binarytree/ConstructStringFromBinaryTree.java
@@ -1,10 +1,5 @@
 package chiskien.datastructures_algorithms.binarytree;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
-
 public class ConstructStringFromBinaryTree {
     static StringBuilder stringBuilder = new StringBuilder();
 
@@ -17,9 +12,11 @@ public class ConstructStringFromBinaryTree {
     }
 
     public static String tree2str(TreeNode root) {
-        preOrderTraversal(root);
-        return stringBuilder.deleteCharAt(stringBuilder.length() - 1).toString();
+        StringBuilder stringBuilder1 = new StringBuilder();
+        dfs(root, stringBuilder1);
+        return stringBuilder1.toString();
     }
+
     public static String preOrderTraversal(TreeNode root) {
         if (root != null) {
             if (root.left != null || root.right != null) {
@@ -38,5 +35,19 @@ public class ConstructStringFromBinaryTree {
             stringBuilder.append(")");
         }
         return stringBuilder.toString();
+    }
+
+    public static void dfs(TreeNode t, StringBuilder res) {
+        if (t == null) return;
+        res.append(t.val);
+        if (t.left == null && t.right == null) return;
+        res.append("(");
+        dfs(t.left, res);
+        res.append(")");
+        if (t.right != null) {
+            res.append("(");
+            dfs(t.right, res);
+            res.append(")");
+        }
     }
 }
