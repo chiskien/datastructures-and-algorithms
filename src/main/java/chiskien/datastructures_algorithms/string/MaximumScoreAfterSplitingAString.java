1513139b09d355a4cabb3476ee77b31b45cc8638
ChisKien <chiskien03214@gmail.com>
2023-12-24 08:48:55 +0700

    URL Queue

---
+++ src/main/java/chiskien/datastructures_algorithms/string/MaximumScoreAfterSplitingAString.java
@@ -0,0 +1,17 @@
+package chiskien.datastructures_algorithms.string;
+
+public class MaximumScoreAfterSplitingAString {
+
+    //Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings (i.e. left substring and right substring).
+    //The score after splitting a string is the number of zeros in the left substring plus the number of ones in the right substring.
+
+    public int maxScore(String s) {
+        int left = 0;
+        int right = 1;
+        for (int i = 0; i < s.length(); i++) {
+
+
+        }
+        return 1;
+    }
+}
