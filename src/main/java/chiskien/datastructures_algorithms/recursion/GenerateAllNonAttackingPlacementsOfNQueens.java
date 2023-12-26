fd04293c6a67f97e59375035a5e47249e631efc0
ChisKien <chiskien03214@gmail.com>
2023-12-26 16:33:57 +0700

    GenerateAllNonAttackingPlacementsOfNQueens

---
+++ src/main/java/chiskien/datastructures_algorithms/recursion/GenerateAllNonAttackingPlacementsOfNQueens.java
@@ -0,0 +1,39 @@
+package chiskien.datastructures_algorithms.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateAllNonAttackingPlacementsOfNQueens {
+
+
+    public List<List<Integer>> nQueens(int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        solveNQueens(n, 0, new ArrayList<>(), result);
+    }
+
+    private void solveNQueens(int n, int row, List<Integer> columnPlacement, List<List<Integer>> result) {
+        if (row == n) {
+            //All queens are legally placed
+            result.add(new ArrayList<>(columnPlacement));
+        } else {
+            for (int col = 0; col < n; col++) {
+                columnPlacement.add(col);
+                if (isValid(columnPlacement)) {
+                    solveNQueens(n, row + 1, columnPlacement, result);
+                }
+                columnPlacement.remove(columnPlacement.size() - 1);
+            }
+        }
+    }
+
+    private boolean isValid(List<Integer> columnPlacement) {
+        int rowId = columnPlacement.size() - 1;
+        for (int i = 0; i < rowId; i++) {
+            int diff = Math.abs(columnPlacement.get(i) - columnPlacement.get(rowId));
+            if (diff == 0 || diff == rowId - 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
